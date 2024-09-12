caab5e7b6568ae626a0d21b6bbaa172db815ee99
himanshutiwari2005 <himanshutiwari10112005@gmail.com>
2024-09-12 18:50:32 +0530

    Libraries
    importing libraries

---
+++ main.py
@@ -0,0 +1,7 @@
+#Importing the libraries required as we go.
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import speech_recognition as sr
+
+#Setting up a data stream
