826522c4cf20414f5f8842c2ab64667c9ec70587
himanshutiwari2005 <himanshutiwari10112005@gmail.com>
2024-09-19 19:44:31 +0530

    extractive summ
    scraped the idea of abstractive Summarizer for now.

--- summarizer/main.py
+++ summarizer/main.py
@@ -1,10 +1,10 @@
 #Importing the libraries required as we go.
 
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
 import speech_recognition as sr
 import requests as rq
 from bs4 import BeautifulSoup as bs
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 #Making a function to Extracting Data from URL using BeautifulSoup4
 def extract_text_from_url(url):
@@ -19,3 +19,28 @@ def extract_text_from_url(url):
     # print(text)
     return text
 
+url = input(str("Input the URL:"))
+text = extract_text_from_url(url)
+
+# Tokenizing the text
+stopwords = set(stopwords.words("English"))
+tokenixed_text = word_tokenize(text)
+
+#Scoring each word in the text by frequency
+table = dict()
+for words in tokenixed_text:
+    #Converting the words to lower
+    words = words.lower()
+    
+    # ignoring stopwords
+    if words in stopwords:
+        continue
+    
+    # Adding the unique word in the dict initialised
+    if words not in table:
+        table[words]=1
+        
+    # Increasing the score when words are repeated
+    if words in table:
+        table[words]+=1
+        
--- summarizer/model_abstract.py
+++
@@ -1,8 +0,0 @@
-# Import libraries as we go
-
-import numpy as np
-import pandas as pd
-import re
-from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-
