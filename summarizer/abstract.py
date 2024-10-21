0fa0a0ca5699bf62772345f77d02f496f69bf2a5
himanshutiwari2005 <himanshutiwari10112005@gmail.com>
2024-10-21 19:28:43 +0530

    Encoding
    Encoding and decoding

--- summarizer/abstract.py
+++ summarizer/abstract.py
@@ -314,3 +314,194 @@ BATCH_SIZE = 64
 
 dataset = tf.data.Dataset.from_tensor_slices((inputs, targets)).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
+# Positional Encoding for adding notion of position among words as unlike RNN this is non-directional
+
+def get_angles(position, i,  d_model):
+    angle_rates = 1/ np.power(10_000, (2*(i//2))/ np.float32(d_model))
+    return position*angle_rates
+
+def positional_encoding(position, d_model):
+    angle_radians = get_angles(
+        np.arange(position)[:,np.newaxis],
+        np.arange(d_model)[np.newaxis, :],
+        d_model
+    )
+    
+    # apply sin to even indices (2i) and cos to odd (2i+1)
+    angle_radians[:,0::2] = np.sin(angle_radians[:,0::2])
+    angle_radians[:,1::2] = np.sin(angle_radians[:,1::2])
+    
+    position_encoding = angle_radians[np.newaxis, ...]
+    
+    return tf.cast(position_encoding, dtype=tf.float32)
+
+"""
+MASKING":
+
+1. Padding mask.
+2. Lookahead mask.
+"""
+
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+def create_lookahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+# Building the model
+
+## Scaled Dot Product
+def scaled_dot_prod_attention(q, k, v, mask):
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+    
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk/tf.math.sqrt(dk)
+    
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+        
+    attention_weights = tf.nn.softmax(scaled_attention_logits, v)
+    
+    output = tf.matmul(attention_weights, v)
+    return output, attention_weights
+
+# Multi-Headed Attention
+
+class MultiHeadAttention(kr.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        
+        assert d_model % self.num_heads == 0
+        
+        self.depth = d_model // self.num_heads
+        
+        self.wq = kr.layers.Dense(d_model)
+        self.wk = kr.layers.Dense(d_model)
+        self.wv = kr.layers.Dense(d_model)
+        
+        self.dense = kr.layers.Dense(d_model)
+        
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+    
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+        
+        q = self.wq(q)
+        k = self.wk(k)
+        v = self.wv(v)
+        
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+        
+        scaled_attention, attention_weight = scaled_dot_prod_attention(q, k, v, mask)
+        
+        scaled_attention = tf.transpose(scaled_attention, perm=[0,2,1,3])
+        
+        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
+        output = self.dense(concat_attention)
+        
+        return output, attention_weight
+    
+# Feed Forward Network
+def point_wise_feed_forward(d_model, dff):
+    return kr.Sequential([
+        kr.layers.Dense(dff, activation='relu'),
+        kr.layers.Dense(d_model)
+    ])
+
+## Fundamental Unit of transformer encoder
+
+class EncodeLayer(kr.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncodeLayer, self).__init__()
+        
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward(d_model, dff)
+        
+        self.norm1 = kr.layers.LayerNormalization(epsilon=1e-6)
+        self.norm2 = kr.layers.LayerNormalization(epsilon=1e-6)
+        
+        self.dropout1 = kr.layers.Dropout(rate)
+        self.dropout2 = kr.layers.Dropout(rate)
+        
+    def call(self, x, training, mask):
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(attn_output, training)
+        out1 = self.norm1(x + attn_output)
+        
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training)
+        out2 = self.norm2(out1 + ffn_output)
+        
+        return out2
+    
+## Fundamental Unit of transformer decoder
+
+class DecodeLayer(kr.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(DecodeLayer, self).__init__()
+        
+        self.mha1 = MultiHeadAttention(d_model, num_heads)
+        self.mha2 = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward(d_model, dff)
+        
+        self.norm1 = kr.layers.LayerNormalization(epsilon=1e-6)
+        self.norm2 = kr.layers.LayerNormalization(epsilon=1e-6)
+        self.norm3 = kr.layers.LayerNormalization(epsilon=1e-6)
+        
+        self.drop1 = kr.layers.Dropout(rate)
+        self.drop2 = kr.layers.Dropout(rate)
+        self.drop3 = kr.layers.Dropout(rate)
+        
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        attn1, attn_weights_block1 = self.mha1(x,x,x,look_ahead_mask)
+        attn1 = self.drop1(attn1, training)
+        out1 = self.norm1(attn1 + x)
+        
+        attn2, attn_weights_block2 = self.mha2(enc_output, enc_output, out1, padding_mask)
+        attn2 = self.drop2(attn2, training)
+        out2 = self.norm2(attn2 + out1)
+        
+        ffn_output = self.ffn(out2)
+        ffn_output = self.drop3(ffn_output, training)
+        out3 = self.norm3(ffn_output + out2)
+        
+        return out3, attn_weights_block1, attn_weights_block2
+    
+# Encoder consisting of Multiple EncoderLayer(s)
+
+class Encoder(kr.layers.Layer):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, maximum_positioning_encoding, rate=0.1):
+        super(Encoder, self).__init__()
+        
+        self.d_model = d_model
+        self.num_layers = num_layers
+        
+        self.embed = kr.layers.Embedding(input_vocab_size, d_model)
+        self.pos_enc = positional_encoding(maximum_positioning_encoding, self.d_model)
+        
+        self.enc_layers = [EncodeLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        
+        self.dropout = kr.layers.Dropout(rate)
+        
+    def call(self, x, training, mask):
+        seq_len = tf.shape(x)[1]
+        
+        x = self.embed(x)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos_enc[:, :seq_len, :]
+        
+        x = self.dropout(x, training)
+        
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, training, mask)
+            
+        return x
+    
