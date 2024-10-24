b6b47601bd3b7081360459256e237e2bfc1f2eba
himanshutiwari2005 <himanshutiwari10112005@gmail.com>
2024-10-24 19:03:24 +0530

    Transformer
    Completed the Decoder Layer and transformer

--- summarizer/abstract.py
+++ summarizer/abstract.py
@@ -505,3 +505,54 @@ class Encoder(kr.layers.Layer):
             
         return x
     
+# Decoder consisting of multiple decoder layer(s)
+
+class Decoder(kr.layers.Layer):
+    def __init__(self, num_layers ,d_model, num_heads, dff, target_vocab_size, maximum_position_encoding, rate=0.1):
+        self.d_model = d_model
+        self.num_layers = num_layers
+        
+        self.embed = kr.layers.Embedding(target_vocab_size, d_model)
+        self.pos = positional_encoding(maximum_position_encoding, d_model)
+        
+        self.dec_layers = [DecodeLayer(d_model, num_heads, dff, rate) for _ in range(num_layers)]
+        self.drop = kr.layers.Dropout(rate)
+        
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        seq_len = tf.shape(x)[1]
+        attention_weigh = {}
+        
+        x = self.embed(x)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x += self.pos[: , :seq_len, :]
+        
+        x = self.drop(x, training)
+        
+        for i in range(self.num_layers):
+            x, block1, block2 = self.dec_layers[i](x, enc_output, training, look_ahead_mask, padding_mask)
+            
+            attention_weigh['decoder_layer{}_block1'. format(i+1)] = block1
+            attention_weigh['decoder_layer{}_block2'. format(i+1)] = block2
+            
+        return x, attention_weigh
+    
+
+# Transformer
+
+class Transformer(kr.Model):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size, target_vocab_size, pe_input, pe_target, rate=0.1):
+        super(Transformer, self).__init()
+        
+        self.encoder = Encoder(num_layers, d_model, num_heads, dff, input_vocab_size, pe_input, rate)
+        self.decoder = Decoder(num_layers, d_model, num_heads, dff, target_vocab_size, pe_target, rate)
+        
+        self.final_layer = kr.layers.Dense(target_vocab_size)
+        
+    def call(self, inp, tar, training, enc_padding_mask, look_ahead_mask, dec_padding_mask):
+        enc_output = self.encoder(inp, training, enc_padding_mask)
+        dec_output, attention_weights = self.decoder(tar, enc_output, training, look_ahead_mask, dec_padding_mask)
+        
+        final_output = self.final_layer(dec_output)
+        
+        return final_output, attention_weights
+    
