67a64f014b4858d56cff4da3b2d8d562082531a6
himanshutiwari2005 <himanshutiwari10112005@gmail.com>
2024-10-27 17:15:24 +0530

    Metrics
    Leaning Schedule and loss metrics

--- summarizer/abstract.py
+++ summarizer/abstract.py
@@ -563,3 +563,23 @@ dff = 512
 num_heads = 8
 epochs = 125
 
+# Adam Optimizer with custom learning rate scheduling
+class CustomLearningSchedule(kr.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps = 4000):
+        super(CustomLearningSchedule, self).__init__()
+        
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.warmup_steps = warmup_steps
+        
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+        
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+    
+# Losses and other metrics
+learning_rates = CustomLearningSchedule(d_model)
+optimizer = kr.optimizers.Adam(learning_rates, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+loss_object = kr.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
